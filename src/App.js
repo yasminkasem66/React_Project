e37768f52dc206d98d19bfe9fafc63b9bb62e266
Mohamed Hamdy <mohamedgelany2030@gmail.com>
2021-11-24 01:59:09 +0200

    add footer2 and jumia info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import JumiaInfo from './Component/Jumia-info/JumiaInfo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer2 from './Component/Footer2/Footer2';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <JumiaInfo/>
+    <Footer2/>
     </div>
   );
 }

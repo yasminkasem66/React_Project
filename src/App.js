9912f2cc4ec8ca64143ea2e7a34c97aa1be13d6d
Mohamed Hamdy <mohamedgelany2030@gmail.com>
2021-11-24 21:53:43 +0200

    import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home';
-
+import Products from './Pages/Products/Products';
 
 function App() {
   return (

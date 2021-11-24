48102472bf0c32a012f388fc80c17c0749e192fc
gaber <gabermohamed772@gmail.com>
2021-11-24 22:29:14 +0200

    gabercommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SmallCard from './Component/SmallCard/SmallCard';
+import pic1 from './assets/imgs/padge1.png'
+import ProductCard from './Component/ProductCard/ProductCard';
+
+
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row container mx-auto">
+      
+      <SmallCard statement="mohamed" pic={pic1} className="col-3" />
+      <SmallCard statement="ahmed" pic={pic1} className="col-3" />
+      <SmallCard statement="ewe" pic={pic1} className="col-3"/>
+      <SmallCard statement="adas" pic={pic1} className="col-3" />
+
+      <ProductCard  pic={pic1} />
+     
     </div>
   );
 }

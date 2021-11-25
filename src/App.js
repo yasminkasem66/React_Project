02c6d1c2143536f9e73a3fa8daf7c8db80286e84
gaber <gabermohamed772@gmail.com>
2021-11-25 10:54:54 +0200

    singleProductPage

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SmallCard from './Component/SmallCard/SmallCard';
-import pic1 from './assets/imgs/padge1.png'
-import ProductCard from './Component/ProductCard/ProductCard';
-
-=======
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/Home/Home';
 import Products from './Pages/Products/Products';
->>>>>>> 84e8a09a0cdb3e4acf13d03be04835f260dbdbfe
+import SingleProduct from './Pages/SingleProduct/SingleProduct';
 
 
 function App() {
 
   
   return (
-<<<<<<< HEAD
-    <div className="row container mx-auto">
-      
-      <SmallCard statement="mohamed" pic={pic1} className="col-3" />
-      <SmallCard statement="ahmed" pic={pic1} className="col-3" />
-      <SmallCard statement="ewe" pic={pic1} className="col-3"/>
-      <SmallCard statement="adas" pic={pic1} className="col-3" />
-
-      <ProductCard  pic={pic1} />
-     
-=======
     <div className="App bg-light">
-      <Home/>
-      {/* <Home /> */}
-      <Products />
+      
+      <SingleProduct/>
+      
 
 
-      <h2>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</h2>
->>>>>>> 84e8a09a0cdb3e4acf13d03be04835f260dbdbfe
+      
     </div>
   );
 }
---
+++ src/Component/ProductDetails/ProductDetails.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function ProductDetails(props) {
+    return (
+        <>
+            <div
+  className="row"
+  style={{
+    backgroundColor: "white",
+    padding: 8,
+    paddingLeft: 20,
+    boxShadow: "0 2px 5px 0 #ededed",
+    borderRadius: 4
+  }}
+>
+  <h5>Product Details</h5>
+  <hr />
+  <ul style={{ fontSize: 14 }}>
+    <li>{props.name}</li>
+    <li>{props.size}</li>
+    <li>{props.specs1}</li>
+    <li>{props.specs2}</li>
+    <li>{props.specs3}</li>
+    <li>{props.specs4}</li>
+    <li id="specifications">
+     {props.notes}
+    </li>
+  </ul>
+</div>
+
+            
+        </>
+    )
+}

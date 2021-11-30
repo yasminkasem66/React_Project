bd05a48ee2e2b667dc1dc32a6f3c368626c83c0d
Mohamed Hamdy <mohamedgelany2030@gmail.com>
2021-11-30 13:25:52 +0200

    login

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Products from "./Pages/Products/Products";
 import SingleProduct from "./Pages/SingleProduct/SingleProduct";
 import Checkout from "./Pages/Checkout/Checkout";
 import Cart from "./Component/Cart/Cart";
-import Checkout from './Pages/Checkout/Checkout';
 import Login from "./Pages/Login/Login";
 import Registration from './Pages/Registration/Registration'
 
@@ -18,7 +17,7 @@ function App() {
       {/* <SingleProduct/> */}
       <Checkout />
 
-      <Products />
+      {/* <Products /> */}
 
       {/* <SingleProduct/> */}
       {/* <Login/> */}
